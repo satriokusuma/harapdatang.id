af232142c17018613c27d844e052f4c635069152
satriokusuma <sir.poteto@gmail.com>
2020-12-07 17:42:28 +0700

    Company Profile Live-1

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+window.addEventListener('load' , () => {
+  const preload = document.querySelector('.preload');
+  setTimeout(() => {
+      preload.classList.add('preload-finish');
+  },1400)
+  
+  });
+
+//==================================NAVBAR=====================================================================//
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ");
+const navbarLinks = document.querySelectorAll(".site-link");
+const navbarImg = document.querySelector(".site-branding");
+const navbarHeader = document.querySelector(".navigation");
+const root = document.getElementsByTagName( 'html' )[0]; // '0' to assign the first (and only `HTML` tag)
+
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+  root.classList.toggle("open");
+  navbarHeader.classList.toggle("open");
+  navbarImg.classList.toggle("close-img");
+}
+
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+
+  }
+}
